1e2ca5242974e6304692a99ec5849f01d8d12c74
josefallasvindas <josefallasvindas@gmail.com>
2017-02-24 02:27:09 -0600

    SETUP
    Basic docs

---
+++ register.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>My first PHP Website</title>
+    </head>
+    <body>
+        <h2>Registration Page</h2>
+        <a href="index.php">Click here to go back<br/><br/>
+        <form action="checklogin.php" method="POST">
+           Enter Username: <input type="text" name="username" required="required" /> <br/>
+           Enter password: <input type="password" name="password" required="required" /> <br/>
+           <input type="submit" value="Register"/>
+        </form>
+    </body>
+</html>
+<?php
+ ?>
+alert("Username has been taken!");</script>'; // Prompts the user
+           Print '<script>window.location.assign("register.php");</script>'; // redirects to register.php
+        }
+    }
+
+    if($bool)
+    {
+        mysql_query("INSERT INTO users (username, password) VALUES ('$username', 'password')"); // inserts value into table users
+        Print '<script>alert("Successfully Registered!");</script>'; // Prompts the user
+        Print '<script>window.location.assign("register.php");</script>'; // redirects to register.php
+    }
+}
+?>
